d8ae1574bed74883b0d6376718fa48296cadccbd
Astemir <amaremkulov96@gmail.com>
2021-06-09 16:01:32 +0300

    Исправления

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,5 @@
 "use strict";
 
-let myMap;
-ymaps.ready(init);
-
-function init() {
-  myMap = new ymaps.Map('map', {
-    center: [59.938635, 30.323118],
-    zoom: 17
-  });
-  const myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {
-    hintContent: 'ул. Большая Конюшенная, д. 19/8'
-  });
-  myMap.geoObjects.add(myPlacemark);
-}
-"use strict";
-
 const phoneInput = document.querySelector('#phone');
 const maskOptions = {
   mask: '+7(000)000-00-00'
