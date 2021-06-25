387855e73b03bb019b107620d3d24965c3aa26a2
metanol596 <54666541+metanol596@users.noreply.github.com>
2021-06-25 12:27:02 +0000

    Deploying to gh-pages from  @ af7de47629c25839eb67d3796fa57fb10c4e1fd8 🚀

---
+++ js/main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const phoneInput = document.querySelector('#phone');
+const maskOptions = {
+  mask: '+7(000)000-00-00'
+};
+const mask = new IMask(phoneInput, maskOptions);
+"use strict";
+
+const openMenuButton = document.querySelector('.page-header__open-menu-button');
+const closeMenuButton = document.querySelector('.main-nav__close-menu-button');
+const menu = document.querySelector('.main-nav');
+const headerBlock = document.querySelector('.page-header--no-js');
+const logo = document.querySelector('.page-header__logo-wrap--no-js');
+const body = document.querySelector('body');
+openMenuButton.addEventListener('click', () => {
+  menu.classList.add('active');
+  body.classList.add('page-body--no-scroll');
+});
+closeMenuButton.addEventListener('click', () => {
+  menu.classList.remove('active');
+  body.classList.remove('page-body--no-scroll');
+});
+
+const removeNojsClass = () => {
+  openMenuButton.classList.remove('page-header__open-menu-button--no-js');
+  closeMenuButton.classList.remove('main-nav__close-menu-button--no-js');
+  headerBlock.classList.remove('page-header--no-js');
+  logo.classList.remove('page-header__logo-wrap--no-js');
+  menu.classList.remove('page-header__main-nav--no-js');
+  menu.classList.remove('main-nav--no-js');
+};
+
+removeNojsClass();
+"use strict";
+
+const menuAnchors = document.querySelectorAll('.main-nav a');
+menuAnchors.forEach(anchor => {
+  anchor.addEventListener('click', evt => {
+    evt.preventDefault();
+    const blockId = anchor.getAttribute('href');
+    document.querySelector(blockId).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
